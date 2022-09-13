df905adc237a69fd54bf91bbfe3389b63cdce423
ahmet-arvas <71208337+Ahmet-Arvas@users.noreply.github.com>
2022-09-13 18:22:52 +0300

    Her acilista kayit klasoru kontrolu

--- preload.js
+++ preload.js
@@ -3,8 +3,8 @@ const os = require("os");
 
 const userHomeDir = os.homedir();
 
-if (!fs.existsSync(userHomeDir+'/Documents/Simobot IDE/')){
-    fs.mkdirSync(userHomeDir+'/Documents/Simobot IDE/', { recursive: true });
+if (!fs.existsSync(userHomeDir+'/Documents/Simobot IDE/Projects')){
+    fs.mkdirSync(userHomeDir+'/Documents/Simobot IDE/Projects', { recursive: true });
 }
 
 if (!fs.existsSync(userHomeDir+'/Documents/Simobot IDE/settings.json')){
